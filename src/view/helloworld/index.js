c5f9bf30f1a9d538ad9ab39f0677ecbaf53df6a9
zhongshuai <81243232@qq.com>
2018-05-08 18:32:15 +0800

    精简多页目录

--- src/view/helloworld/index.js
+++ src/view/helloworld/index.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
 
-import VueRouter from 'vue-router'
-import routes from './router.js'
 import http from 'src/lib/http'
 import 'lib-flexible/flexible.js'
+import '../../assets/scss/style.scss'
 
 import main from './main.vue'
-
-Vue.use(VueRouter)
 
 // 1. 开启debug模式
 Vue.config.debug = true
@@ -17,16 +14,11 @@ Vue.prototype.$http = http
 
 // 3. 创建 router 实例，然后传 `routes` 配置
 // 你还可以传别的配置参数, 不过先这么简单着吧。
-const router = new VueRouter({
-  mode: 'hash', // 强制使用hash模式进行导航，不能指望服务器支持history
-  routes: routes // （缩写）相当于 routes: routes
-})
 
 // 4. 创建和挂载根实例。
 // 记得要通过 router 配置参数注入路由，
 // 从而让整个应用都有路由功能
 const app = new Vue({
-  render: h => h(main),
-  router: router
+  render: h => h(main)
 })
 app.$mount('#app')
--- src/view/helloworld/main.vue
+++ src/view/helloworld/main.vue
@@ -1,27 +1,195 @@
-<!-- 基础目录组件，用于渲染路由页面 -->
 <template>
-  <div>
-    <!-- 路由出口 -->
-    <!-- 路由匹配到的组件将渲染在这里 -->
-    <div>
-      <!--实际页面内容-->
-      <router-view></router-view>
-    </div>
-  </div>
+  <!-- vue要求每个组件中都有且只能有一个根元素 -->
+  <div class="wrapper">
+		<div class="make-money">
+			<div class="banner">
+				<h1 class="title">如何用章鱼星球赚钱?
+				</h1>
+				<p class="summary">章鱼星球是一款基于区块链的高性能云盘
+				</p>
+				<p class="summary">只需插电联网，就能每天自动为您产生积分星
+				</p>
+			</div>
+			<div class="make-money-main">
+				<div class="stars">
+					<div class="money-box">
+						<div class="money-box-hd">
+							<h1 class="tit">赚钱只需三步</h1>
+						</div>
+						<div class="money-box-bd money-box-bd1">
+							<ul>
+								<li>
+									<div class="num num1"></div>
+									<p>前往星球联盟官网
+										<a href="http://m.2345.org#buyStar">2345.org</a>购买章鱼星球</p>
+								</li>
+								<li>
+									<div class="num num2"></div>
+									<p>
+										确保章鱼星球插电联网并接入容量≥32G的硬盘或U盘
+									</p>
+									<img class="pic" src="../../assets/img/m-money-pic1.jpg" alt="pic1">
+									<p class="tip">
+										* 章鱼星球指示灯为蓝色时，即为联网成功
+									</p>
+								</li>
+								<li>
+									<div class="num num3"></div>
+									<p>打开APP进入“章鱼星球”页，点击绑定按钮并扫描章鱼星球背后二维码即可绑定
+									</p>
+									<img class="pic" src="../../assets/img/m-money-pic2.jpg" alt="pci2">
+									<div class="footer">
+										<p>
+											绑定成功后，
+										</p>
+										<p>
+											章鱼星球就能为您自动产生积分星啦！
+										</p>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="money-box">
+						<div class="money-box-hd">
+							<h1 class="tit">如何查看收益</h1>
+						</div>
+						<div class="money-box-bd money-box-bd2">
+							<p class="tit-tip">在首页，点击“我的积分星”即可查看收益
+							</p>
+							<img class="pic" src="../../assets/img/m-money-pic3.jpg" alt="pci3">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
-<style lang="scss">
-@import '../../assets/scss/style.scss';
+<style lang='scss'>
 /*  style标签之内会被作为css进行处理，所以需要使用css的注释形式   */
 /*  加上scoped可以将css的作用效果限定在该组件之内 */
+.make-money {
+  background: #525df8;
+  .banner {
+    background:url('../../assets/img/make-money-banner.jpg') 0 0 no-repeat;
+    $h: 500px;
+    height: $h;
+    background-size: cover;
+    text-align: center;
+    color: #fff;
+    .title {
+      padding: 85px 0px 65px;
+      color: #fff;
+      font-weight: bold;
+      font-size: 53px;
+    }
+    .summary {
+      font-size: 26px;
+      line-height: 1.5;
+    }
+    img {
+      vertical-align: top;
+      max-width: 100%;
+    }
+  }
+  &-main {
+    padding: 0px 30px 40px;
+    margin-top: -170px;
+    .stars {
+      background:url(../../assets/img/star.png);
+      background-position: 87% 94%; // background-size: 720) auto;
+    }
+  }
+  .money-box {
+    background: #fff;
+    border-radius: 10px;
+    margin-bottom: 50px;
+    padding-top: 30px;
+    &-hd {
+      text-align: center;
+      .tit {
+        margin: 0 auto;
+        display: inline-block;
+        width: 483px;
+        height: 122px;
+        line-height: 122px;
+        background:url(../../assets/img/plan-spr.png) 0 0 no-repeat;
+        background-size: cover;
+        font-size: 32px;
+        color: #fff;
+      }
+    }
+    &-bd {
+      &.money-box-bd1 {
+        padding: 20px 40px 60px 75px;
+      }
+      &.money-box-bd2 {
+        text-align: center;
+        font-size: 28px;
+      }
+      line-height: 44px;
+      .pic {
+        margin: 50px 0px;
+        width: 500px;
+      }
+      .footer {
+        color: #525df8;
+        text-align: center;
+        font-weight: bold;
+      }
+      .tit-tip {
+        margin-top: 20px;
+        text-align: center;
+      }
+      li {
+        position: relative;
+        padding-left: 20px;
+        margin: 40px 0px;
+        font-size: 28px;
+        color: #000;
+        a {
+          text-decoration: underline;
+          color: #525df8;
+        }
+        .num {
+          position: absolute;
+          left: -33px;
+          display: inline-block;
+          width: 33px;
+          height: 45px;
+          &.num1 {
+            background:url('../../assets/img/m-money-list1.png') 0 0 no-repeat;
+            background-size: cover;
+          }
+          &.num2 {
+            background:url(../../assets/img/m-money-list2.png) 0 0 no-repeat;
+            background-size: cover;
+          }
+          &.num3 {
+            background:url(../../assets/img/m-money-list3.png) 0 0 no-repeat;
+            background-size: cover;
+          }
+        }
+        .tip {
+          color: #999;
+          font-size: 24px;
+        }
+      }
+    }
+  }
+}
 </style>
 
 <script>
 export default {
   data: function() {
     return {
-      msg: 'Use Vue 2.0 Today!'
+      msg: 'Welcome To Use Vue 2.0 Today!'
     }
+  },
+  components: {
   }
 }
 </script>
+
